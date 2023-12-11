b63d4530bf7340355da7d3c990fddfac11c575b0
Irsnag <104862524+Irsnag@users.noreply.github.com>
2023-12-11 21:04:51 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from 
+
+is_prod = True # production mode or not
+
+if is_prod:
+    model = seq2seqModel.load(path_to_save_models + 'pretrained_moodle.pt')
+
+    to_test = ['I am a student.',
+               'I have a red car.',  # inversion captured
+               'I love playing video games.',
+                'This river is full of fish.', # plein vs pleine (accord)
+                'The fridge is full of food.',
+               'The cat fell asleep on the mat.',
+               'my brother likes pizza.', # pizza is translated to 'la pizza'
+               'I did not mean to hurt you', # translation of mean in context
+               'She is so mean',
+               'Help me pick out a tie to go with this suit!', # right translation
+               "I can't help but smoking weed", # this one and below: hallucination
+               'The kids were playing hide and seek',
+               'The cat fell asleep in front of the fireplace']
+
+    for elt in to_test:
+        print('= = = = = \n','%s -> %s' % (elt, model.predict(elt)))
